ff42f62c552b2f0be1c8076d53f675427e17b438
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-06-08 00:24:56 -0700

    completed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
  /* Static Methods and Properties */
 /* ----------------------------- */
 
+
 // There is only one Governor in the state; add static methods and properties you'd expect a Governor to have.
 // Log the properties and test the methods (but don't instantiate an object of the class).
 
@@ -18,16 +19,61 @@ class Governor {
     static terms = ["1975-1983", "2011-2019"];
 
     static campaign() {
-        console.log("campaign");
+        console.log(`Vote for ${this.name} to govern ${this.state}!`);
     }
     static accumulateWealth() {
-        console.log("accumulate wealth");
+        console.log(`The people of ${this.state} can trust ${this.name}!`);
     }
 }
-console.log(Governor);
+
+
+// console.log(Governor);
+// Governor.campaign();
+// Governor.accumulateWealth();
+
 
 class Person {
-    constructor () {
-        // something
+    constructor (name, dob, health) {
+        this.name = name;
+        this.dob = dob;
+        this.health = health; // something
+    }
+    greet() {
+        return(`Hello my name is ${this.name}. Be prepared for my birthday on ${this.dob}.`);    
+    }
+    reportHealth() {
+        return(`My health is currently ${this.health} which explains why I fell like I do.`);
     }
 }
+
+class PostalWorker extends Person {
+    constructor(name, dob, hireDate, empId) {
+        super(name, dob);
+        this.hireDate = hireDate;
+        this.empId = empId;
+    }
+    clockIn() {
+        console.log("What a great day to be alive and working!");
+    }
+    sortMail() {
+        console.log("This mail won't sort itself. I'm on it!");
+    }
+    clockOut() {
+        console.log("Another day, another dollar.");
+    }
+}
+
+class Chef extends Person {
+    constructor(name, dob, speciality) {
+        super(name, dob);
+       this.speciality = speciality;
+    }
+    makeLunch() {
+        console.log(`Time to make my world-famous ${this.speciality}`);
+        }
+}
+
+const someBody1 = new PostalWorker("Mc Gee", 19941031, 20220505, 666);
+const someBody2 = new PostalWorker("Susan", 19940522, 20180105, 6660);
+const someBody3 = new Chef("Bobo", 19941031, "Cake");
+const someBody4 = new Chef("Jojo", 19941031, "Squid");
